f53c971f7849cd150a0a140b3e6053e3c2d5d24b
Diogo Santos <diogo.cerqueirasantos@gmail.com>
2026-08-31 22:36:53 -0300

    Adiciondo personagem

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Personagem p1 = new Personagem();
+
+        p1.nome = "Luffy";
+        p1.idade = 18;
+        p1.classe = "Pirata";
+        p1.poder = "Borracha";
+
+
+    }
+}
---
+++ Personagem.java
@@ -0,0 +1,7 @@
+public class Personagem {
+
+    public String nome;
+    public int idade;
+    public String classe;
+    public String poder;
+}
