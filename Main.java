b6a12903e6657c0db401b960e7a49a3a8ca5d836
Reginaldo Pereira <rneto8281@gmail.com>
2026-09-01 22:18:10 -0300

    Atualizado  montagem de personagem e lista de escolha de gosto musical

--- Main.java
+++ Main.java
@@ -148,7 +148,7 @@ public class Main {
 
         System.out.print("Digite a idade : ");
         p5.idade = entrada.nextInt();
-
+        entrada.nextLine(); // Consumir a quebra de linha após nextInt()
         System.out.print("Digite a classe : ");
         p5.classe = entrada.nextLine();
 
@@ -161,12 +161,60 @@ public class Main {
         if(resposta.equalsIgnoreCase("Sim")){
 
             p5.imprime();
+
+
         }else{
 
             System.out.println("Então procure o que fazer viu ");
         }
 
         //agora ver e quebre o codigo viu palhaço
+
+
+        String [] musica = {"Rock", "Pop", "Rap", "Trap", "Mpb"};
+    for (int i = 0; i < musica.length; i++){
+
+        System.out.println(musica[i]);
+
+    }
+    System.out.println("---------------------");
+
+    System.out.println("Escolha um estilo musical : " );
+    System.out.println("1 - M1");
+    System.out.println("2 - M2");
+    System.out.println("3 - M3");
+    System.out.println("4 - M4");
+    System.out.println("5 - M5");
+    System.out.print("Escolha : ");
+    String leitor2 = entrada.nextLine();
+
+    do {
+
+        switch (leitor2){
+
+            case "1" :
+                System.out.println("Você escolheu o estilo musical : " + musica[0]);
+                break;
+            case "2" :
+                System.out.println("Você escolheu o estilo musical : " + musica[1]);
+                break;
+            case "3" :
+                System.out.println("Você escolheu o estilo musical : " + musica[2]);
+                break;
+            case "4" :
+                System.out.println("Você escolheu o estilo musical : " + musica[3]);
+                break;
+            case "5" :
+                System.out.println("Você escolheu o estilo musical : " + musica[4]);
+                break;
+
+        }
+
+        System.out.print("Deseja escolher mais algum estilo musical : ");
+        leitor2 = entrada.nextLine();
+}
+while (leitor2.equalsIgnoreCase("Sim"));
+
 
 
 
